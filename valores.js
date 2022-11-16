b6b730abcbe469c268c505c1ce290e175e83b1a1
juank <juanprogram06@gmail.com>
2022-11-15 19:48:14 -0500

    Dia 2

---
+++ valores.js
@@ -0,0 +1,24 @@
+
+ //VALORES PRIMITIVOS O BASICOS
+
+    // Existen diferentes tipos de datos, la forma de declarar datos numericos en javascript es escribiendolos de manera normal
+    40
+
+    // como declarar datos de tipo texto 
+    "Juan Camilo"
+
+    // datos de tipo booleano
+    true
+    false
+
+    // datos vacios o valores placeholder
+    null
+    undefined
+
+// VALORES TIPO OBJETOS
+
+    // Arrays: Tipo de dato que permite almacenar dentro de si mismo mas datos de otro tipo
+    [1,2, "a", true]
+
+    // OBJETO: Este da valor a un ojeto
+    {nombre: "juan"}
---
+++ variables.js
@@ -0,0 +1,23 @@
+// VARIABLES: son la representacion de algun lugar en memoria que vamos a reservar para guardar un valor
+   
+    // Como declarar variables
+    //Existen dos formas de declarar las variables
+
+    //Con const podemos crear una variable la cual no podra ser reinicializada (declarar mas de una vez) ni reasignar su valor (reescribir su valor dentro de otro bloque de codigo)
+    const nombre = "Camilo"
+
+    //Con let podemos crear una variable que se puede resignar en un bloque de codigo especifico,pero no se puede reinicializar
+    let edad
+
+    // Como inicializar variables
+    edad = 30;
+
+    // variables de array
+    const elementos = ["cpmputador",1 , "celular"]
+
+    // variables objeto
+
+    const persona = {
+        nombre: "Juan",
+        ciudad: "Bogotá"
+    }
