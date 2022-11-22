d9ef1b373c50f1308f102268d55e921d3149a8c2
juank <juanprogram06@gmail.com>
2022-11-22 15:26:42 -0500

    archivo arrays y juego con ciclos if y siwtch

---
+++ arrays.js
@@ -0,0 +1,39 @@
+// los arrays son tipos de datos los cuales almacenan mas datos dentro de ellos mismos
+    //EJEMPLO
+    let fruits =["apple", "strawberry","pineapple","passionfruit"]
+
+    //Forma de ver el array
+    console.log(fruits);
+
+    //Como ver la longitud del array
+    console.log (fruits.length);
+
+    //Los arrays inician en 0 y la forma de ver cada uno de sus elementos es:
+    console.log(fruits[3]);
+    //siendo el numero que va entre los [], el numero del elemanto el cual queremos ver
+
+
+        //¿CÓMO ADICIONAR DATOS A LA COLA DE UN ARRAY?
+         let moreFruits = fruits.push("banana");
+
+         //¿CÓMO QUITAR DATOS DE LA COLA DEL ARRAY?
+         let lessFruits = fruits.pop("banana");
+
+
+
+         //Adicionar archivos al inicio del array 
+         let newLength = fruits.unshift("banana");
+
+         //quitar datos deicio de array
+         let unLength = fruits.shift("banana");
+
+
+// Esta es la forma de como saber la posicion de un elemnato dentro del array cuando se su nombre
+
+let position = fruits.indexOf("pineapple");
+
+//Despues debemos llamar la funcion en la consola para ver la posicion de este
+position;
+
+
+         
---
+++ piedra.js
@@ -0,0 +1,42 @@
+// Uso de if
+let jugador
+let cpu 
+
+function juego (cpu,jugador) {
+    if (cpu == jugador){
+        console.log("empate");
+    } else if (cpu == "tijeras" && jugador == "papel" || cpu == "papel" && juagdor == "piedra" || cpu =="piedra" && jugador =="tijeras"){
+        consolej.log("perdiste");
+    } else if (cpu == "tijeras" && jugador == "piedra" || cpu == "papel" && juagdor == "tijera" || cpu =="piedra" && jugador =="papel") {
+        console.log("ganaste")
+    } else {
+        console.log("Elije un valor que sea valido")
+    }
+}
+
+    //inicializar funcion
+    juego (cpu.jugador);
+
+//Uso de switch
+
+let player;
+let machine;
+
+function playground (player, machine){
+
+switch (true) {
+        case player == machine:
+            console.log("empate");
+            break;
+        case machine == "tijeras" && player == "papel" || machine == "papel" && player == "piedra" || machine =="piedra" && player =="tijeras":
+            console.log("perdiste");
+            break;
+        case machine == "tijeras" && player == "piedra" || machine == "papel" && player == "tijera" || machine =="piedra" && player =="papel":
+            console.log("ganaste");
+            break;
+        default:
+            console.log("ingresa un valor valido");
+            break;
+    }
+}
+
